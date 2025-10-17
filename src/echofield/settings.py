de6a7e154b73376baa4bda8dc61e7152140e496e
Yevhen Zavoloka <yevhen.zavoloka@gmail.com>
2025-10-17 23:19:56 +0300

    Configures dynamic allowed hosts
    Updates allowed hosts setting to fetch from environment
    variable, enabling easier configuration for different
    environments.

--- src/echofield/settings.py
+++ src/echofield/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
